533b83b5244cf89434c91b0897313e861da0fc4b
meesu99 <meesu99@naver.com>
2025-07-28 13:53:20 +0900

    chore: initial commit

---
+++ 20250728/java_practice/Calculate.java
@@ -0,0 +1,13 @@
+public class Calculate {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+
+        int sum = x + y;
+        int product = x * y;
+
+        System.out.println("합계: " + sum);
+        System.out.println("곱셈: " + product);
+    }
+    
+}
--- 20250728/java_practice/MyInfo.java
+++ 20250728/java_practice/MyInfo.java
@@ -4,10 +4,14 @@ public class MyInfo {
         int age = 27;
         double height = 170.5;
         boolean isStudent = false;
+        String company = "KT";
+        String position = "NW 플랫폼 개발";
 
         System.out.println("이름: " + name);
         System.out.println("나이: " + age);
         System.out.println("키: " + height);
         System.out.println("학생 여부: " + isStudent);
+        System.out.println("회사: " + company);
+        System.out.println("직무: " + position);
     }
 }
